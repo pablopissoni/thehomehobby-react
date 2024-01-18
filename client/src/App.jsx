7b2410d4864291138c2b00c943d930ce6dfda7b7
pablopissoni <pablopissoni@gmail.com>
2024-01-18 18:41:08 -0300

    se agrego rutas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./components/page/HomePage.jsx";
+import { NavBar } from "./components/NavBar.jsx";
+import { Login } from "./components/page/Login.jsx";
+
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline bg-slate-200">
-    Hello world!
-  </h1>
-  )
+    <div>
+        <NavBar/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login/>} />
+        {/* <Route path="/detail/:id" element={<Login/>} /> */}
+      </Routes>
+    </div>
+  );
 }
 
 export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+// ---- Rutas ----
+import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+
+  // <React.StrictMode>
+  // <App />
+  //  </React.StrictMode>,
+);
