f5e7598461f56b19c21808a0f94c7c6932dccaa9
pablopissoni <pablopissoni@gmail.com>
2024-01-24 19:55:59 -0300

    se anulo la funcion Redux temporalmente

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
 // App.jsx
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from './store/configureStore';
-import HomePage from './components/HomePage';
+// import { Provider } from 'react-redux';
+// import store from './store/configureStore';
+import { NavBar } from './components/NavBar';
+import { HomePage } from './components/HomePage';
 
 const App = () => {
   return (
-    <Provider store={store}>
+    // <Provider store={store}>
       <div>
+        <NavBar />
         <HomePage />
       </div>
-    </Provider>
+    // </Provider>
   );
 };
 
--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,23 +1,21 @@
 // components/HomePage.jsx
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchProducts } from '../actions/productActions';
-import ProductList from './ProductList';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchProducts } from '../actions/productActions';
+// import ProductList from './ProductList';
 
-const HomePage = () => {
-  const dispatch = useDispatch();
-  const products = useSelector((state) => state.products);
+export const HomePage = () => {
+  // const dispatch = useDispatch();
+  // const products = useSelector((state) => state.products);
 
-  useEffect(() => {
-    dispatch(fetchProducts());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(fetchProducts());
+  // }, [dispatch]);
 
   return (
     <div>
       <h1>Home Page</h1>
-      <ProductList products={products} />
+      {/* <ProductList products={products} /> */}
     </div>
   );
 };
-
-export default HomePage;
