1f355bbbce5a03de0cd9333d84381c03d8ddecdb
pablopissoni <pablopissoni@gmail.com>
2024-01-24 23:17:39 -0300

    se limito el uso de NavBar en ciertas URL personalizadas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 // App.jsx
 import React from 'react';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation} from "react-router-dom";
 // import { Provider } from 'react-redux';
 // import store from './store/configureStore';
 import { Login } from './components/page/Login';
@@ -8,10 +8,17 @@ import { NavBar } from './components/NavBar';
 import { HomePage } from './components/HomePage';
 
 const App = () => {
+
+  // Obtengo las ubicaciones y guardo en una variable las que quiero ignorar despues
+  const { pathname } = useLocation(); 
+  const ignorePaths = ['/login'];
+  const isIgnored = ignorePaths.includes(pathname);
+  //-------------------------------
+
   return (
     // <Provider store={store}>
     <div>
-        <NavBar/>
+      {!isIgnored && <NavBar/>} {/* No aparece en las URLs de "ignorePaths" */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login/>} />
