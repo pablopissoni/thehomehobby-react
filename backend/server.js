1bbeee7a587a68878ec672ab700c92557131b88a
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2024-01-26 12:01:15 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Configuración de la conexión a la base de datos
 const dbConfig = {
   host: "thehome.cf42mm20c12e.us-east-1.rds.amazonaws.com",
   user: "adbize",
@@ -13,44 +14,22 @@ const dbConfig = {
   port: 3306,
 };
 
-const connection = mysql.createConnection(dbConfig);
+app.use(bodyParser.json());
 
-// Conectar a la base de datos
-connection.connect((err) => {
-  if (err) {
-    console.error("Error al conectar a la base de datos:", err);
-    throw err;
-  }
-  console.log("Conexión a la base de datos establecida");
-});
+// Conectar a la base de datos utilizando createPool
+const pool = mysql.createPool(dbConfig);
 
-// Ruta para obtener los primeros dos productos
-app.get("/api/productos", (req, res) => {
-  const query = "SELECT * FROM newschema.productos LIMIT 2;";
+// Usar las rutas definidas en routes.js
+app.use(
+  "/api",
+  (req, res, next) => {
+    req.pool = pool; // Pasar el pool a través de la solicitud
+    next();
+  },
+  routes
+);
 
-  console.log("Consulta SQL:", query);
-
-  connection.query(query, (error, results) => {
-    if (error) {
-      console.error("Error en la consulta a la base de datos:", error);
-      res.status(500).json({ error: "Error en el servidor" });
-      return;
-    }
-
-    // Imprimir los resultados en la consola
-    console.log("Resultados de la consulta:", results);
-
-    // Enviar los resultados como respuesta al cliente
-    res.json(results);
-  });
-});
-
-// Capturar eventos de error de la conexión
-connection.on("error", (err) => {
-  console.error("Error en la conexión a la base de datos:", err);
-});
-
-// Iniciar el servidor
+// Resto del código...
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
