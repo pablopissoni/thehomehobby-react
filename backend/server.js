46620a5a4d60a3e1ca7a3d9e33fc97bcc7891bd2
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2024-01-30 15:06:42 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql");
-const router = require("./routes");
+const router = require("./routes/productsRoutes");
+const dbConnection = require("./dbConfig");
 
 const app = express();
 const port = 3001;
@@ -11,27 +11,25 @@ app.use(cors());
 
 app.use(express.json());
 
-// Configuración de la conexión a la base de datos
-const connection = mysql.createConnection({
-  host: "thehome.cf42mm20c12e.us-east-1.rds.amazonaws.com",
-  user: "adbize",
-  password: "Adbize13",
-  database: "newschema",
+// Middleware para pasar la conexión a las rutas
+app.use((req, res, next) => {
+  req.dbConnection = dbConnection;
+  next();
 });
 
-// Conexión a la base de datos
-connection.connect((error) => {
+// Configuración de la conexión a la base de datos
+dbConnection.connect((error) => {
   if (error) {
     console.error("Error al conectar con la base de datos:", error);
+    process.exit(1);
   } else {
     console.log("Conexión exitosa a la base de datos");
+
+    // Iniciar el servidor solo después de que la conexión a la base de datos sea exitosa
+    app.listen(port, () => {
+      console.log(`Servidor iniciado en el puerto ${port}`);
+    });
   }
 });
 
-// Rutas
-app.use("/", router(connection));
-
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor iniciado en el puerto ${port}`);
-});
+app.use("/", router);
