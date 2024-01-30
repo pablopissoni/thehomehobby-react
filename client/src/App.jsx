fab08dd68bb5a896aee8a6c72ce3fd3b22ec43da
pablopissoni <pablopissoni@gmail.com>
2024-01-30 14:34:15 -0300

    fix merge app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,21 +3,14 @@ import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 // import { Provider } from 'react-redux';
 // import store from './store/configureStore';
-<<<<<<< Updated upstream
+
 import { Login } from "./components/page/Login";
 import { NavBar } from "./components/NavBar";
 import { HomePage } from "./components/HomePage";
 import { Details } from "./components/page/Details";
 import { Register } from "./components/page/Register";
 import { ContactUs } from "./components/page/ContactUs";
-=======
-import { Login } from './components/page/Login';
-import { NavBar } from './components/NavBar';
-import { HomePage } from './components/HomePage';
-import { Details } from './components/page/Details';
-import { Register } from './components/page/Register';
-// import { Footer } from './components/Footer';
->>>>>>> Stashed changes
+
 
 const App = () => {
   // Obtengo las ubicaciones y guardo en una variable las que quiero ignorar despues
