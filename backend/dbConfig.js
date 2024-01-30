b827dc67e90f04430bf71af558ab50b53ac1501d
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2024-01-30 15:06:28 -0300

    Create dbConfig.js

---
+++ backend/dbConfig.js
@@ -0,0 +1,14 @@
+// dbConfig.js
+const mysql = require("mysql");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+// No necesitas llamar a connection.connect() aquí
+
+module.exports = connection;
