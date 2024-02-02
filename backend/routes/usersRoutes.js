de8177b436c42b9d7c35823a3e3ff56ab839a087
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2024-02-02 03:09:05 -0300

    Update usersRoutes.js

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,10 +1,15 @@
 // usersRoute.js
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/usersControllers");
+const {
+  registerUser,
+  loginUser,
+  confirmAccount,
+} = require("../controllers/usersControllers");
 
 // Rutas relacionadas con la gestión de usuarios
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/confirm", confirmAccount);
 
 module.exports = router;
