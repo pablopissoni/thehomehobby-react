e4352188ce540e4f48f8b0775e2dddf8d813091d
pablopissoni <pablopissoni@gmail.com>
2024-02-03 13:26:03 -0300

    Mejoras de estilo y mensaje

--- client/src/components/page/Login.jsx
+++ client/src/components/page/Login.jsx
@@ -4,12 +4,27 @@ import LogoGrande from "../../assets/logo The Home Hobby.svg";
 import axios from "axios";
 
 export const Login = () => {
+  //* URL local o deploy
+  const isLocal = window.location.href.includes("localhost");
+  const urlLogin = isLocal
+    ? "http://localhost:3001/users/login"
+    : "XXXXXXXXXXX DEPLOY"; //! COLOCAR RUTA DEPLOY
+
+  const url = isLocal ? "http://localhost:5173/" : "XXXXXXXXXXX DEPLOY"; //! COLOCAR RUTA DEPLOY
+  // URL local o deploy
+
+  //* ---- HOOKs --------
   const [loginData, setLoginData] = useState({
     email: "",
     password: "",
   });
-  const [successMessage, setSuccessMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState({
+    message: "",
+    error: "",
+  });
+  // ---- HOOKs --------
 
+  //* --- HANDLES ----------------
   const handleInput = (event) => {
     const { name, value } = event.target;
     setLoginData({
@@ -20,35 +35,34 @@ export const Login = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
     try {
       // Send login request to the backend
-      const response = await axios.post(
-        "http://localhost:3001/users/login",
-        loginData
-      );
+      const response = await axios.post(urlLogin, loginData);
 
       console.log("Login Response:", response.data);
 
       // Set success message
-      setSuccessMessage("Login successful!");
+      setSuccessMessage({ message: "Login successful!" });
 
       // Redirect user after 5 seconds
       setTimeout(() => {
-        window.location.href = "http://localhost:5173/";
+        window.location.href = url;
       }, 5000);
     } catch (error) {
+      setSuccessMessage({ error: "Login the email or password is wrong!" });
       console.error("Error during login:", error);
       // Handle login error, display a message, etc.
     }
   };
+  // --- HANDLES ----------------
 
   // Clear success message after component unmounts
-  useEffect(() => {
-    return () => {
-      setSuccessMessage("");
-    };
-  }, []);
+  // useEffect(() => {
+  //   return () => {
+  //     setSuccessMessage("");
+  //   };
+  // }, []);
+  console.log("🚀 ~ Login ~ successMessage:", successMessage);
 
   return (
     <div className="bg-slate-100 flex items-center justify-center h-screen">
@@ -58,29 +72,38 @@ export const Login = () => {
           <div className="w-full">
             <div className="card w-full lg:flex">
               {/* Logo */}
-              <a href="/" className="flex p-4 lg:w-1/3">
-                <img src={LogoGrande} alt="Logo" />
+              <a
+                href="/"
+                className="flex p-4 lg:w-1/3 transition-transform duration-300 hover:scale-110"
+              >
+                <img className="" src={LogoGrande} alt="Logo" />
               </a>
               {/* Text card */}
               <div className="p-4 lg:w-2/3">
                 <a href="/" className="noble-ui-logo block mb-2">
-                  <h2 className="text-2xl font-bold">
+                  <h2 className="text-2xl font-bold transition-transform duration-300 hover:scale-105">
                     The <span className="text-red-700">Home Hobby </span>
                   </h2>
                 </a>
                 {/* Form */}
-                <form className="forms-sample" onSubmit={handleSubmit}>
+                <form
+                  className="forms-sample lg:w-[400px] lg:w-auto"
+                  onSubmit={handleSubmit}
+                >
                   {/* Input for Email */}
                   <div className="mb-3">
-                    <label htmlFor="userEmail" className="form-label block">
+                    <label
+                      htmlFor="userEmail"
+                      className="form-label block transition-transform duration-500 hover:translate-x-2"
+                    >
                       Email address
                     </label>
                     <input
                       type="email"
                       name="email"
-                      className="form-control mt-2 border border-gray-200 rounded-sm lg:w-2/3"
+                      className="form-control mt-1 border border-gray-200 rounded-sm w-full pl-2 outline-none focus:border-2 focus:border-b-cyan-500"
                       id="userEmail"
-                      placeholder="  Email"
+                      placeholder="Email"
                       required
                       autoFocus
                       value={loginData.email}
@@ -90,35 +113,49 @@ export const Login = () => {
 
                   {/* Input for Password */}
                   <div className="mb-3">
-                    <label htmlFor="userPassword" className="form-label block">
+                    <label
+                      htmlFor="userPassword"
+                      className="form-label block transition-transform duration-500 hover:translate-x-2"
+                    >
                       Password
                     </label>
                     <input
                       type="password"
                       name="password"
-                      className="form-control mt-2 border border-gray-200 rounded-sm lg:w-2/3"
+                      className="form-control mt-1 border border-gray-200 rounded-sm w-full pl-2  outline-none focus:border-2 focus:border-b-cyan-500"
                       id="userPassword"
                       autoComplete="current-password"
-                      placeholder="  Password"
+                      placeholder="Password"
                       required
                       value={loginData.password}
                       onChange={handleInput}
                     />
                   </div>
-
                   {/* Button for Login */}
                   <div className="lg:flex justify-between lg:w-2/3 mt-5 lg:px-2">
-                    <div className="lg:w-1/3 hidden lg:flex items-center justify-center bg-blue-500 hover:bg-sky-700 h-10  rounded-sm">
+                    <div className="lg:w-2/5 hidden leading-none lg:flex items-center justify-center text-center bg-blue-500 hover:bg-sky-500 hover:shadow-xl h-10  rounded-sm">
                       <a href="/register" className="text-white ">
                         Create an account
                       </a>
                     </div>
-                    <div className="lg:w-1/3 flex items-center justify-center bg-blue-500  hover:bg-sky-700 h-10 rounded-sm">
-                      <button type="submit" className="boton text-white ">
+                    {/* <div className="lg:w-1/3 flex items-center justify-center bg-blue-500  hover:bg-sky-500 hover:shadow-xl h-10 rounded-sm"> */}
+                      <button type="submit" className="boton text-white w-full lg:w-1/3 flex items-center justify-center bg-blue-500  hover:bg-sky-500 hover:shadow-xl h-10 rounded-sm">
                         Login
                       </button>
                     </div>
-                  </div>
+                  {/* </div> */}
+                  {/* Success Message */}
+                  {successMessage?.message && (
+                    <div className="text-green-600 text-center mt-3 lg:ml-4">
+                      {successMessage.message}
+                    </div>
+                  )}
+                  {/* Error Message */}
+                  {successMessage?.error && (
+                    <div className="text-red-600 mt-3 lg:ml-4">
+                      {successMessage.error}
+                    </div>
+                  )}
 
                   {/* Link for Forgot Password */}
                   <div className="text-end mt-3 flex justify-center lg:w-2/3">
@@ -129,13 +166,6 @@ export const Login = () => {
                       Forgot your password?
                     </a>
                   </div>
-
-                  {/* Success Message */}
-                  {successMessage && (
-                    <div className="text-green-600 text-center mt-3">
-                      {successMessage}
-                    </div>
-                  )}
                 </form>
               </div>
             </div>
@@ -151,7 +181,7 @@ export const Login = () => {
         </div>
 
         {/* Link for Register - Hidden in Desktop Mode */}
-        <div className="mt-3 bg-white lg:hidden border border-gray-300 rounded-md cursor-pointer text-center py-2">
+        <div className="mt-3 bg-white lg:hidden border border-gray-300 hover:bg-sky-300 hover:shadow-xl rounded-md cursor-pointer text-center py-2">
           <a href="/register" className="text-dark ">
             Create an account
           </a>
