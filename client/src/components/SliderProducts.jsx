a37070d5a8672b7cfe5f9cfd57efa2e4d936ca2d
MotionSoft05 <kevinbbarrios05@gmail.com>
2024-02-08 15:55:00 -0300

    Update SliderProducts.jsx

--- client/src/components/SliderProducts.jsx
+++ client/src/components/SliderProducts.jsx
@@ -1,64 +1,67 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CardProduct } from "./CardProduct";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-// import required modules
 import { Pagination, Navigation } from "swiper/modules";
 
-// --- IMG ---
-import sliderDesktop1 from "../assets/slider-desktop1.png";
-// import sliderDesktop2 from "../assets/slider-desktop2.png";
-// import sliderDesktop3 from "../assets/slider-desktop3.png";
-// import sliderMobile1 from "../assets/slider-mobile1.png";
-// import sliderMobile2 from "../assets/slider-mobile2.png";
-// import sliderMobile3 from "../assets/slider-mobile3.png";
-
 export const SliderProducts = ({ products }) => {
-  // const propsTest = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  // const productos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  // const productos = products;
+  const [slidesPerView, setSlidesPerView] = useState(4); // Valor predeterminado para dispositivos no móviles
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Ajusta slidesPerView dependiendo del ancho de la ventana
+      if (window.innerWidth < 768) {
+        setSlidesPerView(1); // Para dispositivos móviles, muestra solo una imagen por vista
+      } else {
+        setSlidesPerView(4); // Para otros dispositivos, muestra 4 imágenes por vista
+      }
+    };
+
+    // Agregar event listener para detectar cambios en el tamaño de la ventana
+    window.addEventListener("resize", handleResize);
+
+    // Llama a handleResize una vez para ajustar el valor inicial
+    handleResize();
+
+    // Eliminar event listener al desmontar el componente
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Se ejecuta solo una vez al montar el componente
 
   return (
-    <>
-      {/* <div className=""> */}
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={false}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        slidesPerView={4}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="mySwiper px-10"
-      >
-        {products?.map((prod, index) => (
-          // <div key={index}>
-          <SwiperSlide key={index}>
-            <CardProduct
-              id={prod?.id}
-              off={prod?.oferta_id}
-              img={prod?.imagen}
-              stars={prod?.stars}
-              stock={prod?.stock}
-              title={prod?.nombre_ingles ? prod?.nombre_ingles : prod?.nombre_es}
-              description={prod?.description}
-              price={prod?.precio_base}
-              priceOff={prod?.priceOff}
-            />
-          </SwiperSlide>
-          // </div>
-        ))}
-      </Swiper>
-      {/* </div> */}
-    </>
+    <Swiper
+      spaceBetween={30}
+      centeredSlides={false}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      slidesPerView={slidesPerView} // Usa el estado slidesPerView
+      navigation={true}
+      modules={[Pagination, Navigation]}
+      className="mySwiper px-10"
+    >
+      {products?.map((prod, index) => (
+        <SwiperSlide key={index}>
+          <CardProduct
+            id={prod?.id}
+            off={prod?.oferta_id}
+            img={prod?.imagen}
+            stars={prod?.stars}
+            stock={prod?.stock}
+            title={prod?.nombre_ingles ? prod?.nombre_ingles : prod?.nombre_es}
+            description={prod?.description}
+            price={prod?.precio_base}
+            priceOff={prod?.priceOff}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
