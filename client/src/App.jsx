4dd9c0131e824162a1e4d3e89d117258bfdd0e73
pablopissoni <pablopissoni@gmail.com>
2024-02-28 16:16:41 -0300

    fix route home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
     <div>
       {!isIgnored && <NavBar />} {/* No aparece en las URLs de "ignorePaths" */}
       <Routes>
-        <Route path="https://thehomehobby-react.vercel.app" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/products/:id" element={<SearchProducts />} />{" "}
         {/* Se renderiza la lista de produtos relacionados con el producto de la busqueda */}
         <Route path="/login" element={<Login />} />
