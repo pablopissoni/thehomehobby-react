58ab8ba9781515f8ba0ef8e1ef79f5e7962b4b25
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2024-03-05 01:16:29 -0300

    Update usersControllers.js

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -2,6 +2,8 @@ const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
 const jwt = require("jsonwebtoken");
 const dbConnection = require("../dbConfig");
 const app = require("../server");
+const bcrypt = require("bcrypt");
+
 const poolData = {
   UserPoolId: process.env.COGNITO_USER_POOL_ID,
   ClientId: process.env.COGNITO_CLIENT_ID,
@@ -37,50 +39,132 @@ async function getToken(req, res) {
   }
 }
 
-//registro
 const registerUser = async (req, res) => {
   try {
     const { name, lastName, phone, email, password } = req.body;
 
-    const attributeList = [
-      new AmazonCognitoIdentity.CognitoUserAttribute({
-        Name: "name",
-        Value: name,
-      }),
-      new AmazonCognitoIdentity.CognitoUserAttribute({
-        Name: "family_name",
-        Value: lastName,
-      }),
-      new AmazonCognitoIdentity.CognitoUserAttribute({
-        Name: "phone_number",
-        Value: phone,
-      }),
-    ];
-
-    userPool.signUp(email, password, attributeList, null, (err, result) => {
+    // Encriptar la contraseña
+    const hashedPassword = await bcrypt.hash(password, 10); // 10 es el número de rondas de hashing
+    // Definir el valor predeterminado para el campo role
+    const defaultRole = "user";
+    // Consultar el último ID insertado en la base de datos
+    dbConnection.query("SELECT MAX(id) AS maxId FROM users", (err, result) => {
       if (err) {
-        console.error("Error al registrar usuario en Cognito:", err);
-        if (err.code === "UsernameExistsException") {
-          return res.status(420).json({ error: "El usuario ya existe" });
-        }
-        return res.status(400).json({ error: "Error al registrar usuario" });
-      }
-      const cognitoUser = result.user;
-
-      if (result.userConfirmed) {
-        const token = result.getIdToken().getJwtToken();
+        console.error(
+          "Error al consultar el último ID en la base de datos:",
+          err
+        );
         return res
-          .status(201)
-          .json({ message: "Usuario registrado exitosamente", token });
-      } else {
-        return res.status(201).json({
-          message: "Usuario registrado. Por favor, confirme su cuenta.",
-        });
+          .status(500)
+          .json({ error: "Error al registrar usuario en la base de datos" });
       }
+
+      const lastId = result[0].maxId || 0; // Si no hay registros, comenzar desde 0
+      const newId = lastId + 1;
+
+      // Insertar usuario en la base de datos MySQL con contraseña encriptada
+      const userData = {
+        id: newId, // Utilizar el nuevo ID
+        name: name,
+        lastName: lastName,
+        phone: phone,
+        email: email,
+        password: hashedPassword,
+        role: defaultRole, // Contraseña encriptada
+      };
+
+      // Continuar con la inserción en la base de datos MySQL
+      dbConnection.query(
+        "INSERT INTO users SET ?",
+        userData,
+        (error, dbResult) => {
+          if (error) {
+            console.error(
+              "Error al insertar usuario en la base de datos:",
+              error
+            );
+
+            let errorCode = 500; // Código de error predeterminado
+
+            // Verificar el tipo de error específico
+            if (error.code === "ER_DUP_ENTRY") {
+              errorCode = 409; // Conflict
+            }
+
+            return res.status(errorCode).json({
+              error: "Error al insertar usuario en la base de datos",
+            });
+          }
+          console.log("Usuario insertado en la base de datos:", dbResult);
+
+          // Realizar el registro en AWS Cognito
+          const attributeList = [
+            new AmazonCognitoIdentity.CognitoUserAttribute({
+              Name: "name",
+              Value: name,
+            }),
+            new AmazonCognitoIdentity.CognitoUserAttribute({
+              Name: "family_name",
+              Value: lastName,
+            }),
+            new AmazonCognitoIdentity.CognitoUserAttribute({
+              Name: "phone_number",
+              Value: phone,
+            }),
+          ];
+
+          userPool.signUp(
+            email,
+            password,
+            attributeList,
+            null,
+            (err, result) => {
+              if (err) {
+                console.error("Error al registrar usuario en Cognito:", err);
+                let errorCode = 500; // Código de error predeterminado
+
+                // Verificar el tipo de error específico
+                if (err.code === "UsernameExistsException") {
+                  errorCode = 409; // Conflict
+                }
+
+                return res.status(errorCode).json({ error: err.message });
+              }
+              const cognitoUser = result.user;
+
+              if (result.userConfirmed) {
+                const token = result.getIdToken().getJwtToken();
+                console.log(
+                  "Usuario registrado exitosamente en Cognito:",
+                  cognitoUser.getUsername()
+                );
+                console.log("Token JWT:", token);
+                return res.status(201).json({
+                  message:
+                    "Usuario registrado exitosamente en la base de datos y en AWS Cognito",
+                  token,
+                });
+              } else {
+                return res.status(201).json({
+                  message:
+                    "Usuario registrado en AWS Cognito. Por favor, confirme su cuenta.",
+                });
+              }
+            }
+          );
+        }
+      );
     });
   } catch (error) {
     console.error("Error al registrar usuario:", error);
-    return res.status(400).json({ error: "Error al registrar usuario" });
+    let errorCode = 500; // Código de error predeterminado
+
+    // Verificar el tipo de error específico
+    if (error.code === "BadRequestException") {
+      errorCode = 400; // Bad Request
+    }
+
+    return res.status(errorCode).json({ error: error.message });
   }
 };
 
