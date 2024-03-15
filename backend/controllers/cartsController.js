db9aeb6e45591265cf944086c504317b7d436157
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2024-03-14 21:22:04 -0300

    Update cartsController.js

--- backend/controllers/cartsController.js
+++ backend/controllers/cartsController.js
@@ -1,5 +1,5 @@
 const addToCart = (req, res, connection) => {
-  const { userId, productId } = req.body;
+  const { userId, productId, quantity } = req.body;
 
   // Verificar si el userId existe en la tabla de usuarios
   const userQuery = "SELECT * FROM users WHERE id = ?";
@@ -14,22 +14,26 @@ const addToCart = (req, res, connection) => {
       return res.status(404).json({ error: "El usuario no existe" });
     }
 
-    // Generar un ID único de 12 caracteres aleatorios
-    const cartId = generateUniqueId();
+    // Generar un ID único de 12 caracteres aleatorios para el elemento del carrito
+    const cartItemId = generateUniqueId();
 
     // Realizar la inserción en la base de datos
     const query =
-      "INSERT INTO carrito (id, userId, productId) VALUES (?, ?, ?)";
-    connection.query(query, [cartId, userId, productId], (error, result) => {
-      if (error) {
-        console.error("Error al insertar en la base de datos:", error);
-        return res.status(500).json({ error: "Error en el servidor" });
+      "INSERT INTO carrito (id, userId, productId, quantity) VALUES (?, ?, ?, ?)";
+    connection.query(
+      query,
+      [cartItemId, userId, productId, quantity],
+      (error, result) => {
+        if (error) {
+          console.error("Error al insertar en la base de datos:", error);
+          return res.status(500).json({ error: "Error en el servidor" });
+        }
+        res.status(201).json({
+          id: cartItemId,
+          message: "Producto agregado al carrito con éxito",
+        });
       }
-      res.status(201).json({
-        id: cartId,
-        message: "Producto agregado al carrito con éxito",
-      });
-    });
+    );
   });
 };
 
@@ -68,24 +72,59 @@ const removeFromCart = (req, res, connection) => {
 const getCartByUserId = (req, res, connection) => {
   const userId = req.params.userId;
 
-  // Consultar todos los productos en el carrito para el userId especificado
   const query = "SELECT * FROM carrito WHERE userId = ?";
-  connection.query(query, [userId], (error, results) => {
+  connection.query(query, [userId], (error, cartResults) => {
     if (error) {
       console.error("Error al consultar el carrito:", error);
       return res.status(500).json({ error: "Error en el servidor" });
     }
 
-    // Si no se encontraron productos en el carrito para el userId proporcionado, devolver un mensaje
-    if (results.length === 0) {
+    if (cartResults.length === 0) {
       return res.status(404).json({
         message:
           "No se encontraron productos en el carrito para el usuario especificado",
       });
     }
 
-    // Devolver los productos encontrados en el carrito
-    res.status(200).json(results);
+    const productIds = cartResults.map((cartItem) => cartItem.productId);
+    console.log("Product IDs:", productIds);
+
+    const productQuery =
+      "SELECT id, imagen, nombre_ingles, oferta_id, precio_base FROM productos WHERE id IN (?)";
+    connection.query(productQuery, [productIds], (error, productResults) => {
+      if (error) {
+        console.error(
+          "Error al consultar la información de los productos:",
+          error
+        );
+        return res.status(500).json({ error: "Error en el servidor" });
+      }
+
+      console.log("Product Results:", productResults);
+
+      const mergedResults = cartResults.map((cartItem) => {
+        const product = productResults.find(
+          (product) => product.id.toString() === cartItem.productId
+        );
+        if (product) {
+          // Agregar la URL base a la imagen del producto
+          product.imagen =
+            "https://thehomehobby.s3.amazonaws.com" + product.imagen;
+        }
+        const mergedItem = {
+          id: cartItem.id,
+          userId: cartItem.userId,
+          productId: cartItem.productId,
+          quantity: cartItem.quantity,
+          product: product, // Aquí se agrega la información completa del producto
+        };
+        return mergedItem;
+      });
+
+      console.log("Merged Results:", mergedResults);
+
+      res.status(200).json(mergedResults);
+    });
   });
 };
 
