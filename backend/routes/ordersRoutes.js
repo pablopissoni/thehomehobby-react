67a7c37a18837994fde8c6d29955ffc0bdbbd027
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2024-03-25 19:13:49 -0300

    Update ordersRoutes.js

--- backend/routes/ordersRoutes.js
+++ backend/routes/ordersRoutes.js
@@ -4,5 +4,9 @@ const ordersController = require("../controllers/ordersController");
 
 // Ruta para crear un nuevo pedido
 router.post("/:userId", ordersController.createOrder);
+// Ruta para obtener todos los pedidos
+router.get("/", ordersController.getAllOrders);
+// Ruta para eliminar un pedido por su ID
+router.delete("/:orderId", ordersController.deleteOrder);
 
 module.exports = router;
